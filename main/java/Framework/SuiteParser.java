e42924a679df9098847b84e1f5b713ace2853014
viralpatel <vir@jsbot.io>
2017-11-29 18:20:19 +0530

    New Version : Initial Commit

---
+++ main/java/Framework/SuiteParser.java
@@ -0,0 +1,4 @@
+package java.Framework;
+
+public class SuiteParser {
+}
---
+++ test/java/Runner/Runner.java
@@ -0,0 +1,16 @@
+package Runner;
+
+public class Runner {
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+
+}
