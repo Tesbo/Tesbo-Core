ae3dffc8b69fbbe8c5348f187b6b265b336085c0
jsbot <roshan@jsbot.io>
2018-03-06 17:45:23 +0530

    Add new unit test.

--- UnitTest/StepTester.java
+++ UnitTest/StepTester.java
@@ -1,13 +1,30 @@
 import framework.StepParser;
+import io.github.bonigarcia.wdm.ChromeDriverManager;
 import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 
 public class StepTester {
 
+    protected WebDriver driver;
+
+    StepTester()
+    {
+        DesiredCapabilities capability = null;
+        capability = DesiredCapabilities.chrome();
+        ChromeDriverManager.getInstance().setup();
+        capability.setJavascriptEnabled(true);
+        driver = new ChromeDriver(capability);
+        driver.manage().window().maximize();
+    }
+
 
       @Test
     public void verifyTextTobeEnterFromStep() {
+
         //Assert.assertTrue(loginFileData.contains("SuiteName: Login"));
         StepParser parser = new StepParser();
         Assert.assertTrue(parser.parseTextToEnter("Enter '' into @emailTextBox").equals(""));
