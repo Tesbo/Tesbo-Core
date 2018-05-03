c2d8edd3cbe881f64c24f276ec2700f8da0ad8a2
jsbot <roshan@jsbot.io>
2018-05-03 14:42:59 +0530

    Add Validation in verify parser

--- main/java/framework/VerifyParser.java
+++ main/java/framework/VerifyParser.java
@@ -4,6 +4,7 @@ import Selenium.Commands;
 import org.json.simple.JSONObject;
 import logger.Logger;
 import org.openqa.selenium.WebDriver;
+import Exception.TesboException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,6 +15,7 @@ public class VerifyParser {
         GetLocator locator = new GetLocator();
         StepParser stepParser = new StepParser();
         Logger logger = new Logger();
+        boolean flag=false;
 
         //Text verification.
         if (verify.toLowerCase().contains("text")) {
@@ -25,12 +27,14 @@ public class VerifyParser {
                      */
                     if (verify.toLowerCase().contains("ignore case")) {
                         assertThat(cmd.findElement(driver, locator.getLocatorValue(test.get("suiteName").toString(), stepParser.parseElementName(verify))).getText()).isEqualToIgnoringCase(stepParser.parseTextToEnter(test,verify));
+                        flag=true;
                     }
                     /**
                      * Verify: @element text is equal "Text"
                      */
                     else {
                         assertThat(cmd.findElement(driver, locator.getLocatorValue(test.get("suiteName").toString(), stepParser.parseElementName(verify))).getText()).isEqualTo(stepParser.parseTextToEnter(test,verify));
+                        flag=true;
                     }
                 }
                 //contains
@@ -40,12 +44,14 @@ public class VerifyParser {
                      */
                     if (verify.toLowerCase().contains("ignore case")) {
                         assertThat(cmd.findElement(driver, locator.getLocatorValue(test.get("suiteName").toString(), stepParser.parseElementName(verify))).getText()).containsIgnoringCase(stepParser.parseTextToEnter(test,verify));
+                        flag=true;
                     }
                     /**
                      * Verify: @element text is contains "Text".
                      */
                     else {
                         assertThat(cmd.findElement(driver, locator.getLocatorValue(test.get("suiteName").toString(), stepParser.parseElementName(verify))).getText()).contains(stepParser.parseTextToEnter(test,verify));
+                        flag=true;
                     }
                 }
             } catch (Exception e) {
@@ -61,6 +67,7 @@ public class VerifyParser {
                  * Verify: @element should displayed
                  */
                     assertThat(cmd.findElement(driver, locator.getLocatorValue(test.get("suiteName").toString(), stepParser.parseElementName(verify))).isDisplayed()).isEqualTo(true);
+                    flag=true;
             } catch (Exception e) {
                 logger.testFailed("Step Failed");
                 throw e;
@@ -71,23 +78,31 @@ public class VerifyParser {
         if (verify.toLowerCase().contains("page title")) {
 
                 //equal
-
+            try {
                 if (verify.toLowerCase().contains("equal")) {
                     /**
                      * Verify : Page Title is equal to ignore case 'Google search'
                      */
                     if (verify.toLowerCase().contains("ignore case")) {
                         assertThat(driver.getTitle()).isEqualToIgnoringCase(stepParser.parseTextToEnter(test,verify));
+                        flag=true;
                     }
                     /**
                      * Verify : Page Title is equal to 'Google search'
                      */
                     else {
                         assertThat(driver.getTitle()).isEqualTo(stepParser.parseTextToEnter(test,verify));
+                        flag=true;
                     }
                 }
-
+            } catch (Exception e) {
+                logger.testFailed("Step Failed");
+                throw e;
+            }
         }
+        if(!flag)
+            throw new TesboException("Step is not define properly.");
     }
+
 }
 
