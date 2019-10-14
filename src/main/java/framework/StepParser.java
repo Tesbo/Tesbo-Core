4ec344d9cfe4a40a1ac8437a9f430d14e0302016
nishil <nishilpatel81@gmail.com>
2019-10-14 18:03:31 +0530

    TES-164 Change keyword for "Navigate Refresh" to "Refresh Page"

--- src/main/java/framework/StepParser.java
+++ src/main/java/framework/StepParser.java
@@ -183,9 +183,11 @@ public class StepParser {
             cmd.getPageSource(driver);
         }
 
-        // Get URL
+        // Open URL
         if (step.toLowerCase().contains("url")) {
-
+            /*
+            Step: Open URL 'http://demo.guru99.com/test/upload/'
+            */
             driver.get(parseTextToEnter(test, step));
         }
 
@@ -195,7 +197,7 @@ public class StepParser {
         }
 
         //navigate
-        if (step.toLowerCase().contains("navigate")) {
+        if (step.toLowerCase().contains("navigate") | (step.toLowerCase().contains("refresh") && step.toLowerCase().contains("page"))) {
             navigateFunction(driver, step);
         }
 
@@ -523,9 +525,10 @@ public class StepParser {
         }
         /**
          * refresh identify.
-         * Step : Navigate refresh
+         * Step : Page Refresh
+         * Step : Refresh Page
          */
-        else if (step.toLowerCase().contains("refresh")) {
+        else if (step.toLowerCase().contains("refresh") && step.toLowerCase().contains("page")) {
             cmd.navigateRefresh(driver);
         }
     }
